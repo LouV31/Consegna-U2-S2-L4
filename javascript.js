ab258a3bb8125856986859066396c6bbb0b8ef68
LouV31 <luigi.valentino99@yahoo.it>
2023-11-30 18:31:18 +0100

    Consegna

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let pulsante = document.getElementById("saldi");
+pulsante.addEventListener("click", function () {
+    const summerContainer = document.getElementById("summerContainer");
+    summerContainer.classList.toggle("d-none");
+});
+
+let pulsante2 = document.getElementById("wSummer");
+pulsante2.addEventListener("click", function () {
+    const welcomeContainer = document.getElementById("welcomeSummerContainer");
+    welcomeContainer.classList.toggle("d-none");
+});
+
+
+
